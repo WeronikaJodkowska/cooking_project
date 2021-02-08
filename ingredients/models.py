0c3812a5a207d0330f08919b5e59137a84d4b891
WeronikaJodkowska <31646812+WeronikaJodkowska@users.noreply.github.com>
2021-02-08 20:27:28 +0300

    change password

--- ingredients/models.py
+++ ingredients/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
+    new_field = models.CharField(max_length=140, default='SOME STRING')
