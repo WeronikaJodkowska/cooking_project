37cd89c83a533c2a1c3fade8dc2c7ec8092940e7
WeronikaJodkowska <31646812+WeronikaJodkowska@users.noreply.github.com>
2021-02-10 20:51:14 +0300

    reset password templates

--- account/urls.py
+++ account/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
     # change password urls
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # reset password urls
+    path('password_reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_confirm'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
