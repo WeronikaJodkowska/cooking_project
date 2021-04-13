c71fc63993a641b1ee77e3d6506fdf718feb65fb
Иодковская Вероника Яновна <V.Iodkovskaya@bns.by>
2021-04-13 15:26:34 +0300

    login registration design

--- account/forms.py
+++ account/forms.py
@@ -6,7 +6,7 @@ from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(forms.ModelForm):
