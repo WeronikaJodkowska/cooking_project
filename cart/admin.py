aa216aeaf12e72020760837d27463256717eac63
WeronikaJodkowska <31646812+WeronikaJodkowska@users.noreply.github.com>
2021-05-21 11:00:27 +0300

    delete some things

--- cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Task)
-
-
-@admin.register(Cart)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ['id', 'recipe', 'user', 'complete', 'created_at']
